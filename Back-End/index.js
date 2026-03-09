986311e3e8737a29304bc7f840fc01b17b2c78da
Prabhakar <prabhakarpanwar808@gmail.com>
2026-03-09 13:03:52 +0530

    fine

--- Back-End/index.js
+++ Back-End/index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://fashioncity001.netlify.app/"],
+    origin: "https://fashioncity001.netlify.app/",
   }),
 );
 mongooseConnect();
